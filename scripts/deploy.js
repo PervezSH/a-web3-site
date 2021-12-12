3b5ee82288b497eaca4b676348b46e9516e92c58
Pervez Shoaib <50441829+PervezSH@users.noreply.github.com>
2021-12-12 15:01:38 +0530

    Added comments of deploy.js
    -explaining each line of code

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,14 +1,18 @@
 const main = async () => {
+    // Grabbing the wallet address of Contract Owner(the person deploying our contract/me)
     const [deployer] = await hre.ethers.getSigners();
+    // Returns the balnce of deployer's wallet
     const accountBalance = await deployer.getBalance();
   
     console.log('Deploying contracts with account: ', deployer.address);
     console.log('Account balance: ', accountBalance.toString());
-  
+    // Compile our contract and generates necessary files under the artifacts directory
     const Token = await hre.ethers.getContractFactory('WavePortal');
+    // Deploying our contract
     const portal = await Token.deploy();
     await portal.deployed();
-  
+
+    // Address where contract is deployed to
     console.log('WavePortal address: ', portal.address);
   };
   
