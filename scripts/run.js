24d38ba140526028378204b360ca3cfe0d196f3b
Pervez Shoaib <50441829+PervezSH@users.noreply.github.com>
2021-12-13 19:49:55 +0530

    test : test our recently added feature
    - make changes on run.js to test recently added feature in local blockchain

--- scripts/run.js
+++ scripts/run.js
@@ -5,30 +5,27 @@ const main = async () => {
     const [owner, randomPerson] = await hre.ethers.getSigners();
     // Compile our contract and generates necessary files under the artifacts directory
     const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
-    const waveContract = await waveContractFactory.deploy();
+    const waveContract = await waveContractFactory.deploy({
+      // Fund the contract so we can send ETH
+      value: hre.ethers.utils.parseEther('0.1'),
+    });
     await waveContract.deployed();
     
     console.log("Contract deployed to:", waveContract.address);
     console.log("Contract deployed by:", owner.address);
 
-    // Getting totalWave count
-    let waveCount;
-    waveCount = await waveContract.getTotalWaves();
-    console.log("A total of %d good people waved at us!", waveCount);
-    
-    // Waving at myself
+    // Get contract balance
+    let contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+    console.log("Contract Balance: ", hre.ethers.utils.formatEther(contractBalance));
+
+    // Wave
     let waveTxn = await waveContract.wave("I'm the Owner");
     await waveTxn.wait(); // Wait for the transaction to be mined
     
-    // Random person waving at us
-    waveTxn = await waveContract.connect(randomPerson).wave("Heyyyyyy!");
-    await waveTxn.wait(); // Wait for the transaction to be mined
+    contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+    console.log("Contract Balance: ", hre.ethers.utils.formatEther(contractBalance));
     
-    // Getting totalWave count again
-    waveCount = await waveContract.getTotalWaves();
-    console.log("A total of %d good people waved at us!", waveCount);
-    
-    // Getting Wave struct
+    // Get all waves
     let allWaves = await waveContract.getAllWaves();
     console.log(allWaves);
   };
